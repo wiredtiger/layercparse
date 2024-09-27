7840356a32c1d69d511c4bf8bae865272c5c1eeb
Yury Ershov <yuriy.ershov@gmail.com>
2024-09-27 16:44:28 +1000

    Multiprocessing

--- test_tokens.py
+++ test_tokens.py
@@ -6,37 +6,58 @@ from pprint import pprint, pformat
 
 from typing import Union, Any, Optional, TYPE_CHECKING, cast, Iterator, TypeAlias, Generator, Iterable
 
+import multiprocessing
+
 def file_content(fname: str) -> str:
     with open(fname) as file:
         return file.read()
 
-def print_statement_from_text(txt: str, offset: int = 0):
+def pf(obj: Any) -> str:
+    return pformat(obj, width=120, compact=False)
+
+def print_statement_from_text(txt: str, offset: int = 0) -> str:
+    a = []
     with ScopePush(offset=offset):
         for st in StatementList.fromText(txt):
             st.getKind()
-            pprint(st, width=120)
+            a.append(pf(st))
+            # a.append(pf(StatementKind.fromTokens(st.tokens)))
             if st.getKind().is_function_def:
                 func = FunctionParts.fromStatement(st)
                 if func:
-                    print("Function:")
-                    pprint(func, width=120)
-                    print("Args:")
-                    pprint(func.getArgs(), width=120)
+                    a.extend(["Function:", pf(func)])
+                    a.extend(["Args:", pf(func.getArgs())])
                     if func.body:
-                        print("Vars:")
-                        pprint(func.getLocalVars(), width=120)
+                        a.extend(["Vars:", pf(func.getLocalVars())])
             elif st.getKind().is_record:
                 record = RecordParts.fromStatement(st)
                 if record:
                     members = record.getMembers()
-                    print("Record:")
-                    pprint(record, width=120)
+                    a.extend(["Record:", pf(record)])
+                    # a.extend(["Members:", pf(members)])
             elif st.getKind().is_extern_c:
                 body = next((t for t in st.tokens if t.value[0] == "{"), None)
                 if body:
-                    print_statement_from_text(body.value[1:-1], offset=body.range[0]+1)
+                    a.append(print_statement_from_text(body.value[1:-1], offset=body.range[0]+1))
+    return "\n".join(a)
 
-for fname in get_files(sys.argv[1]):
-    print(f" === File: {fname}")
+def print_statement_from_file(fname: str) -> str:
+    a = []
+    a.append(f" === File: {fname}")
     with ScopePush(file=File(fname)):
-        print_statement_from_text(file_content(fname))
+        a.append(print_statement_from_text(file_content(fname)))
+    return "\n".join(a)
+
+
+def main():
+    # for fname in get_files(sys.argv[1]):
+    #     print_statement_from_file(fname)
+
+    multiprocessing.set_start_method('fork')  # 'fork' is faster than 'spawn'
+    with multiprocessing.Pool() as pool:
+        for res in pool.starmap(print_statement_from_file, ((f,) for f in get_files(sys.argv[1]))):
+            print(res)
+
+
+if __name__ == "__main__":
+    main()
